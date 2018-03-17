44a5429df89c13a6087306f1a69992fe0bf9c392
hadaralon <hadaron98@gmail.com>
2018-03-17 19:59:35 +0200

    improved board checker

--- game_board.py
+++ game_board.py
@@ -1,11 +1,13 @@
 class GameBoard:
-    def __init__(self, size):
+    def __init__(self, size, board_layout):
         self.size = size
         if self.size > 0:
             self.okay = True
         else:
             self.okay = False
             raise IndexError
+        self.board_layout = board_layout
+        # TODO: initialize an empty board....
 
     def draw_board(self):
         length = (self.size * 2) + 1
@@ -19,8 +21,24 @@ class GameBoard:
                 print(*board_list[i])
         return
 
-    # def check_board(self, board):
-    #     win_check = False
-    #     for i in range(self.size):
-    #         for k in range(self.size):
-    #             if board[]
+    def check_board(self, board):
+        """
+        checks board layout for win
+        TODO: only working on first lineq column- fix it again Tony...
+        :param board:
+        :return:
+        """
+        win_check = False
+        for i in range(self.size - 2):
+            for k in range(self.size - 2):
+                if (board[i][k] == board[i][k + 1] and board[i][k + 1] == board[i][k + 2]) and board[i][k] != ' ':
+                    win_check = True
+                elif (board[i][k] == board[i + 1][k] and board[i + 1][k] == board[i + 2][k]) and board[i][k] != ' ':
+                    win_check = True
+                elif (board[i][k] == board[i + 1][k + 1] and board[i + 1][k + 1] == board[i + 2][k + 2])\
+                        and board[i][k] != ' ':
+                    win_check = True
+                elif (board[i][k+2] == board[i + 1][k + 1] and board[i + 1][k + 1] == board[i + 2][k])\
+                        and board[i][k] != ' ':
+                    win_check = True
+        return win_check
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from game_board import GameBoard
 
 
 def main():
-    my_board = GameBoard(0)
+    my_board = GameBoard(3, [['X', ' ', 'X'],
+                             ['X', 'O', 'X'],
+                             ['O', 'O', 'X']])
     my_board.draw_board()
-
+    print(my_board.check_board(my_board.board_layout))
 
 main()
 
